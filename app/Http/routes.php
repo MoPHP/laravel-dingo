44423c7f6bb525c58bd0af58c001e50dcb924bb1
mocept <dzm1846907072@gmail.com>
2016-08-08 20:50:45 +0800

    有token后才能调用对应 api

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -24,6 +24,9 @@ $api = app('Dingo\Api\Routing\Router');
 $api->version('v1', function ($api) {
     $api->group(['namespace' => 'App\Api\Controller'], function ($api) {
         $api->post('user/login', 'AuthController@authenticate');
-        $api->get('lessons', 'LessonsController@index');
+        // jwt.auth 对应 kernel 中的 jwt.auth
+        $api->group(['middleware' => 'jwt.auth'], function ($api) {
+            $api->get('lessons', 'LessonsController@index');
+        });
     });
 });
