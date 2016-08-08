aca30b9ece5d537bc70829f84290636e8a49c91a
mocept <dzm1846907072@gmail.com>
2016-08-08 21:52:26 +0800

    oauth Migrated: 2014_04_24_110151_create_oauth_scopes_table Migrated: 2014_04_24_110304_create_oauth_grants_table Migrated: 2014_04_24_110403_create_oauth_grant_scopes_table Migrated: 2014_04_24_110459_create_oauth_clients_table Migrated: 2014_04_24_110557_create_oauth_client_endpoints_table Migrated: 2014_04_24_110705_create_oauth_client_scopes_table Migrated: 2014_04_24_110817_create_oauth_client_grants_table Migrated: 2014_04_24_111002_create_oauth_sessions_table Migrated: 2014_04_24_111109_create_oauth_session_scopes_table Migrated: 2014_04_24_111254_create_oauth_auth_codes_table Migrated: 2014_04_24_111403_create_oauth_auth_code_scopes_table Migrated: 2014_04_24_111518_create_oauth_access_tokens_table Migrated: 2014_04_24_111657_create_oauth_access_token_scopes_table Migrated: 2014_04_24_111810_create_oauth_refresh_tokens_table

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,6 +18,7 @@ class Kernel extends HttpKernel
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
         // \App\Http\Middleware\VerifyCsrfToken::class,
+        \LucaDegasperi\OAuth2Server\Middleware\OAuthExceptionHandlerMiddleware::class,
     ];
 
     /**
@@ -31,5 +32,9 @@ class Kernel extends HttpKernel
         // 'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'jwt.auth' => 'Tymon\JWTAuth\Middleware\GetUserFromToken',
         'jwt.refresh' => 'Tymon\JWTAuth\Middleware\RefreshToken',
+        'oauth' => \LucaDegasperi\OAuth2Server\Middleware\OAuthMiddleware::class,
+        'oauth-user' => \LucaDegasperi\OAuth2Server\Middleware\OAuthUserOwnerMiddleware::class,
+        'oauth-client' => \LucaDegasperi\OAuth2Server\Middleware\OAuthClientOwnerMiddleware::class,
+        'check-authorization-params' => \LucaDegasperi\OAuth2Server\Middleware\CheckAuthCodeRequestMiddleware::class,
     ];
 }
--- config/app.php
+++ config/app.php
@@ -138,6 +138,8 @@ return [
         Illuminate\View\ViewServiceProvider::class,
         Dingo\Api\Provider\LaravelServiceProvider::class,
         Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
+        LucaDegasperi\OAuth2Server\Storage\FluentStorageServiceProvider::class,
+        LucaDegasperi\OAuth2Server\OAuth2ServerServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -195,7 +197,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
         'JWTAuth'   => Tymon\JWTAuth\Facades\JWTAuth::class,
-        'JWTFactory'=> Tymon\JWTAuth\Facades\JWTFactory::class
+        'JWTFactory'=> Tymon\JWTAuth\Facades\JWTFactory::class,
+        'Authorizer' => LucaDegasperi\OAuth2Server\Facades\Authorizer::class,
 
     ],
 
