31f95d572879a25c536a63e00f9ea035b8079f59
mocept <dzm1846907072@gmail.com>
2016-08-31 15:17:50 +0800

    middleware

--- app/Api/v1/Controller/PostController.php
+++ app/Api/v1/Controller/PostController.php
@@ -10,18 +10,16 @@ namespace App\Api\V1\Controller;
 
 
 use App\Post;
-use Illuminate\Support\Facades\Gate;
 
 class PostController extends BaseController
 {
     public function getPostById($postId){
         $post = Post::findOrFail($postId);
         // $user = \Auth::user();
-        // \Auth::loginUsingId($user->id); // 以uid的为 的登录
-        $this->authorize('update', $post); // 
+        $this->authorize('update', $post); //
         /*$user = \Auth::user();
         $user->hasRole($permission->roles);*/
-/*        if(Gate::denies('show-post', $post)){
+        /* if(Gate::denies('show-post', $post)){
             abort(403, 'sorry');
         }*/
 
